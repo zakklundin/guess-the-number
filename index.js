a7395cf48a7ddb1fdfa78988b649ac7032c42bdc
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-11-02 14:05:19 +0100

    löst till steg 3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Read instructions. And remove this line.
+function randomInt(n){
+    return Math.floor(Math.random() * n) + 1
+}
+console.log(randomInt(100))
+
+function getUserGuess(){
+    const inputValue = document.getElementById("user-input").value
+    return parseInt(inputValue, 10)
+    //", 10" menar att det ska tolkas i basen 10
+}
+document.addEventListener("keyup", function (a){
+    if (a.key === "Enter") {
+        const theGuess = getUserGuess()
+        console.log(theGuess)
+    }
+})
